6189cb16c205ced27b7f8afe2ff51c17533ad069
Joshua V Sherman <joshua.v.sherman@gmail.com>
2017-02-28 11:41:05 -0500

    added a put by ID route and checkedOutBy attribute for books

--- model/book/book-schema.js
+++ model/book/book-schema.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
+// const criteria = new Schema({
+//   key: String, // TODO: use constants
+//   value: String // TODO: use constants
+// });
+
+// const checkedOutBy = new Schema({
+//   foreignId: mongoose.Schema.ObjectId,
+//   criteria: [criteria]
+// });
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
@@ -15,8 +24,16 @@ const bookSchema = new Schema({
   siteLocation: { type: String, required: false },
   numberOfCopies: { type: Number, required: false },
   access: { type: String, required: false },
-  comments: { type: String, required: false }
+  comments: { type: String, required: false },
+  checkedOutBy: { type: String, required: false }
 });
 
+// bookSchema.methods = {
+//   bookCheckOut(userId) {
+//       return this.checkedOutBy.some(function(checkedOut) {
+//         return checkedOut.foreignId === userId;
+//       })
+//   }
+// };
 
 module.exports = mongoose.model('Book', bookSchema);
