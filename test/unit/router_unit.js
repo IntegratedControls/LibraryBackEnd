638b3819c30cc06bddbd56fe4620172d8bd0ea1e
Joshua V Sherman <joshua.v.sherman@gmail.com>
2017-03-15 15:24:32 -0400

    updated circle.yml, added TODO to router_unit test

--- test/unit/router_unit.js
+++ test/unit/router_unit.js
@@ -1,33 +1,34 @@
-
-const  mock = mockgoose(mongoose);
-
-mock.then(() => {
-  process.env.CORSisON = false;
-  global.server = require('../../index');
-
-  done();
-});
-
-mock.catch(err => {
-  // "use strict";
-  console.log(err);
-});
-
-
-describe('the route redirect', () => {
-  beforeEach((done) => {
-
-    done();
-  });
-
-  it('should redirect to / if CORS is true and allowURL is incorrect', (done) => {
-    chai.request(server)
-    .get('/book/getall')
-    .set({ origin: 'http://bogus.com' })
-    .end((err, res) => {
-
-      expect(res).to.have.status(200);
-      done();
-    });
-  });
-});
+// //TODO fake server with Sinon
+// Server2 = require('../../index');
+// const  ServerMock = sinon.createFakeServer();
+//
+// mock.then(() => {
+//   process.env.CORSisON = true;
+//
+//
+//   done();
+// });
+//
+// mock.catch(err => {
+//   // "use strict";
+//   console.log(err);
+// });
+//
+//
+// describe('the route redirect', () => {
+//   beforeEach((done) => {
+//
+//     done();
+//   });
+//
+//   it('should redirect to / if CORS is true and allowURL is incorrect', (done) => {
+//     chai.request(FAKEserver)
+//     .get('/book/getall')
+//     .set({ origin: 'http://bogus.com' })
+//     .end((err, res) => {
+//
+//       expect(res).to.have.status(200);
+//       done();
+//     });
+//   });
+// });
